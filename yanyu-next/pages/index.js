56b53b43ed54fcea01f924c958f5bf7e6e8c4686
yanyu <yuyan802@outlook.com>
2022-04-15 00:49:04 +1000

    login success

--- yanyu-next/pages/index.js
+++ yanyu-next/pages/index.js
@@ -1,50 +1,90 @@
 import { Layout } from "antd";
 import { Radio } from "antd";
 import { Typography } from "antd";
-import styles from "./home.module.css";
 import { Row, Col } from "antd";
-import { Space } from "antd";
 import "antd/dist/antd.css";
-import {
-  Form,
-  Input,
-  InputNumber,
-  Cascader,
-  Select,
-  Checkbox,
-  Button,
-  AutoComplete,
-} from "antd";
+import { Form, Input, Checkbox, Button } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { AES } from "crypto-js";
+import { useRouter } from "next/router";
 
 const { Title } = Typography;
-const { Header, Footer, Sider, Content } = Layout;
+
+const baseURL = "http://cms.chtoma.com/api";
+const axiosInstance = axios.create({
+  baseURL,
+  withCredentials: true,
+  responseType: "json",
+});
 
 export default function Home() {
-  const onFinish = () => {
-    console.log("cool");
+  const router = useRouter();
+  const onFinish = async (values) => {
+    try {
+      const response = await axiosInstance.post(
+        "/login",
+        JSON.stringify({
+          email: values.email,
+          password: AES.encrypt(values.password, "cms").toString(),
+          role: values.roll,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response.data.data));
+      const accessToken = response.data.data.token;
+      const userRole = response.data.data.role;
+      localStorage.setItem("accessToken", JSON.stringify(accessToken));
+      localStorage.setItem("userRole", JSON.stringify(userRole));
+      if (accessToken && userRole) {
+        router.push("/successlogin");
+      }
+    } catch (err) {
+      if (!err?.response) {
+        console.log("No Server Response");
+      } else if (err.response?.status === 400) {
+        console.log("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        console.log("Unauthorized");
+      } else {
+        console.log("Login Failed");
+      }
+    }
   };
+
   return (
     <>
-      <Row align="left">
+      <Row>
         <Col span={8}></Col>
 
         <Col span={8}>
-          <Title>Course Management Assistant</Title>
-          <Radio.Group defaultValue="student" style={{ marginTop: 16 }}>
-            <Radio.Button value="student">Student</Radio.Button>
-            <Radio.Button value="teacher">Teacher</Radio.Button>
-            <Radio.Button value="manager">Manager</Radio.Button>
-          </Radio.Group>
+          <div
+            style={{ display: "flex", justifyContent: "center", marginTop: 10 }}
+          >
+            <Title>Course Management Assistant</Title>
+          </div>
+
           <Form
             name="normal_login"
             className="login-form"
             initialValues={{
               remember: true,
+              roll: "student",
             }}
             onFinish={onFinish}
-            style={{ marginTop: 16 }}
+            style={{ marginTop: 10 }}
           >
+            <Form.Item label="" name="roll">
+              <Radio.Group style={{ marginTop: 16 }}>
+                <Radio.Button value="student">Student</Radio.Button>
+                <Radio.Button value="teacher">Teacher</Radio.Button>
+                <Radio.Button value="manager">Manager</Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+
             <Form.Item
               name="email"
               label=""
---
+++ yanyu-next/pages/successlogin.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function successlogin() {
+  return <div>success login!!</div>;
+}
